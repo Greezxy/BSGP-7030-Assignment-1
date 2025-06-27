466462eb398f7da99cdac1f73228800b33c6cde5
Weiyi Gong <weiyidg@ascend-rw01.ten.osc.edu>
2025-06-27 00:03:44 -0400

    Add 3 new files

---
+++ hello_bash.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Hello World in Bash
+# This notebook demonstrates a Bash script using Jupyter's `%%bash` magic.
+
+# In[6]:
+
+
+get_ipython().run_cell_magic('bash', '', 'echo "Hello, World from Bash!"\n')
+
